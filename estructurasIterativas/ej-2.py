4bdc43c8df142ff071ff6bb2d669c600f44bd2b5
ana luz <146685869+AnaAguirre77@users.noreply.github.com>
2024-05-25 20:14:26 -0300

    Add files via upload

---
+++ estructurasIterativas/ej-2.py
@@ -0,0 +1,22 @@
+"""
+Mostrar sólo los números pares desde 0 hasta el número ingresado (input). 
+Nota: para saber si un número es par hacer i % 2 == 0)"""
+
+"""
+Analisis:
+Entrada:
+num E Enteros
+Salida:
+contador i E Enteros
+"""
+
+num = int(input("Ingrese un numero entero: "))
+i = 0 
+
+while i <= num:
+  print(i)
+  i = i + 2 
+
+
+
+
