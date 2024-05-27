d2af92c9364c1a28ea13d91e32c3376b3dfa50fe
ana luz <146685869+AnaAguirre77@users.noreply.github.com>
2024-05-26 21:09:11 -0300

    Add files via upload

---
+++ estructurasDeDatos/miPrimerDiccionario.py
@@ -0,0 +1,16 @@
+
+"""Guardar en un diccionario los datos de una persona: nombre, apellido, dni, email, fecha de nacimiento."""
+
+nombre = input("Ingrese su nombre: ")
+
+apellido = input("Ingrese su apellido: ")
+
+dni = input("Ingrese su dni: ")
+
+email = input("Ingrese su email: ")
+
+fechaNacimiento = input("Ingrese su fecha de nacimiento en el formato dd/mm/aaaa: ")
+
+persona1 = {"nombre": nombre, "apellido": apellido, "dni": dni, "email":email, "fecha de Nacimiento":fechaNacimiento}
+
+print(persona1)
