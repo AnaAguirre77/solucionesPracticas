e0671e8058db1bc13b63e7ee98a8335d47c9add9
ana luz <146685869+AnaAguirre77@users.noreply.github.com>
2024-05-26 21:20:45 -0300

    Update miPrimerDiccionario.py

--- estructurasDeDatos/miPrimerDiccionario.py
+++ estructurasDeDatos/miPrimerDiccionario.py
@@ -11,6 +11,6 @@ email = input("Ingrese su email: ")
 
 fechaNacimiento = input("Ingrese su fecha de nacimiento en el formato dd/mm/aaaa: ")
 
-persona1 = {"nombre": nombre, "apellido": apellido, "dni": dni, "email":email, "fecha de Nacimiento":fechaNacimiento}
+persona1 = {"nombre": nombre, "apellido": apellido, "dni": dni, "email":email, "fecha_de_Nacimiento":fechaNacimiento}
 
 print(persona1)
