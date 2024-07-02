9467caba145642b40b7efd237b252bad93aab2d7
ana luz <146685869+AnaAguirre77@users.noreply.github.com>
2024-07-02 13:44:22 -0300

    Delete estructurasDeDatos/7.2-1.py

--- estructurasDeDatos/7.2-1.py
+++
@@ -1,9 +0,0 @@
-"""
-Pedir al usuario que ingrese 10 nombres de personas (input en un ciclo) no repetidas, guardarlos en una lista y mostrarlos. 
-"""
-
-nombres = input("Ingrese 10 nombres de personas diferentes - sin repetir: ")
-
-lista = [nombres]
-
-print(lista)
